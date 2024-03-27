bd8fe518f4af6e04ef9d653b499da31d4660bed1
bunveren <berenunveren0@gmail.com>
2024-03-27 22:58:10 +0300

    finished testutil

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -7,11 +7,28 @@ import org.junit.Test;
 
 public class TestCompute {
   Compute c;
-
+  //When you add your unit tests to TestCompute,
+  //you are going to use mockito to mock MessageQueue interface.
   @Test
   public void example() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
     assertTrue(true);
   }
+
+  @Test
+  public void testSizeZero(){
+
+  }
+
+  @Test
+  public void testNotContains(){
+
+  }
+
+  @Test
+  public void testMain(){
+
+  }
+
 }
--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,4 +12,26 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void testArgLengthOne(){
+    assertFalse(c.compute(new int[]{1}));
+  }
+
+  @Test
+  public void testArgLengthEven(){
+    assertFalse(c.compute(new int[]{2,4,6,8}));
+    assertFalse(c.compute(new int[]{2,4}));
+  }
+
+  @Test
+  public void testArgZero(){
+    assertFalse(c.compute(new int[]{2,0,4,6}));
+    assertFalse(c.compute(new int[]{3,4,0,5}));
+  }
+
+  @Test
+  public void testComputeMain(){
+    assertTrue(c.compute(new int[]{2,4,6}));
+  }
 }
