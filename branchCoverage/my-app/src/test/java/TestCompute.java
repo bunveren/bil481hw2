a9dbb8326111d12d215a998750816240a0401aaa
bunveren <berenunveren0@gmail.com>
2024-03-28 19:51:42 +0300

    finished mocking

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -19,6 +19,16 @@ public class TestCompute {
   @Test
   public void testSizeZero(){
     MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(0);
+    when(mq.contains(anyString())).thenReturn(true);
+
+    Compute a = new Compute(mq);
+    assertEquals(-1, a.countNumberOfOccurrences("test"));
+  }
+
+  @Test
+  public void testNotContains(){
+    MessageQueue mq = mock(MessageQueue.class);
     when(mq.size()).thenReturn(3);
     when(mq.contains(anyString())).thenReturn(false);
 
@@ -27,13 +37,18 @@ public class TestCompute {
   }
 
   @Test
-  public void testNotContains(){
-
-  }
-
-  @Test
   public void testMain(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(5);
+    when(mq.getAt(0)).thenReturn("t");
+    when(mq.getAt(1)).thenReturn("e");
+    when(mq.getAt(2)).thenReturn("s");
+    when(mq.getAt(3)).thenReturn("t");
+    when(mq.getAt(4)).thenReturn("t");
+    when(mq.contains("t")).thenReturn(true);
 
+    Compute a = new Compute(mq);
+    assertEquals(3, a.countNumberOfOccurrences("t"));
   }
 
 }
